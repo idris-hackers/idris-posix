1157bd7a839e12fc31a381e72528dcc4e9989365
κeen <3han5chou7@gmail.com>
2019-01-06 00:57:46 +0900

    do not use modern APIs

--- src/stat_wrappers.c
+++ src/stat_wrappers.c
@@ -3,13 +3,13 @@
 inline int idris_posix_stat_file_type(char *filename) {
   struct stat buffer;
   stat(filename, &buffer);
-  return buffer.st_mode & S_IFMT;
+  return buffer.st_mode;
 }
 
 inline int idris_posix_is_directory(char *filename) {
-  return idris_posix_stat_file_type(filename) == S_IFDIR;
+  return S_ISDIR(idris_posix_stat_file_type(filename));
 }
 
 inline int idris_posix_is_file(char *filename) {
-  return idris_posix_stat_file_type(filename) == S_IFREG;
+  return S_ISREG(idris_posix_stat_file_type(filename));
 }
